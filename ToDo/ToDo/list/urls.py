b0753519c94cb8a662d71580585429ad5aae2630
Brent <bg240711@ohio.edu>
2018-02-07 15:49:50 -0500

    updating list app urls.py

--- ToDo/ToDo/list/urls.py
+++ ToDo/ToDo/list/urls.py
@@ -1,1 +1,13 @@
+from django.conf.urls import urls
+from . import views
 
+app_name = 'list'
+urlpatterns = [
+
+    url(r'^$',views.index, name='index'),
+    url(r'^addform/$',views.addform, name='addform'),
+    url(r'^add/$',views.add, name='add'),
+    url(r'^viewlist/$',views.viewlist, name='viewlist'),
+    url(r'^change_tasks/$',views.change_tasks, name='change_tasks'),
+    url(r'^create_account/$',views.create_account, name='create_account'),
+]
